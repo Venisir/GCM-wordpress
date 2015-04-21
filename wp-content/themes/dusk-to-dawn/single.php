22055d738ec0053bdaf64e4f5c85776c990e5ecb
Venisir <sithis77@gmail.com>
2015-04-21 21:08:33 +0200

    Hola Mundo!

---
+++ wp-content/themes/dusk-to-dawn/single.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package Dusk_To_Dawn
+ */
+get_header(); ?>
+
+<div id="primary">
+	<div id="content" class="clear-fix" role="main">
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php the_field('autor'); ?>
+		<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php dusktodawn_content_nav( 'nav-below' ); ?>
+		<?php comments_template(); ?>
+
+	<?php endwhile; // end of the loop. ?>
+
+	</div><!-- #content -->
+</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
