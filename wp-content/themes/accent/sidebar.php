29313eaf4de1e7db2b5a844207c48b05c99a5da2
nimanym <nimanym@gmail.com>
2015-04-29 22:03:15 +0200

    TO EL ADD CHAVAAL

--- wp-content/themes/accent/sidebar.php
+++ wp-content/themes/accent/sidebar.php
@@ -11,5 +11,6 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <div id="secondary" class="widget-area" role="complementary">
+	<?php echo do_shortcode('[searchandfilter fields="search,category"  post_types="product"]'); ?>
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
